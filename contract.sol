f149d252dfbbd777e8ca7e375f2c66b970473f8b
Dima aka PathFinder <123137984+0xPathFinder@users.noreply.github.com>
2023-07-17 04:07:11 -0700

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -3,21 +3,20 @@ pragma solidity 0.8.0;
  
 contract Multisig{ 
  
-    address public admin; 
- 
-    uint public requiredConfirmations;  
- 
+    address public admin;
+    uint public requiredConfirmations;
     uint public ownerSize;
- 
     address second;
- 
+    bool isCreateTxIdCalled;
+    uint txID;
+
     mapping(address => bool) public isOwner; 
     mapping(address => bool) public isConfirmedBy; 
     mapping(uint => uint) public currentNumConfirmations; 
     mapping(uint => Transactions) public executedTransactions; 
  
     event Deposit(address indexed _from, uint indexed _value, uint indexed _at); 
-    event Confirmed(address indexed _from); // event to confirm() 
+    event Confirmed(address indexed _from); 
     event Revoked(address indexed _from);
     event Withdrawn(address indexed _to, uint indexed _value, uint indexed _at); 
  
@@ -44,7 +43,7 @@ contract Multisig{
         isOwner[_owner] = true; 
  
         requiredConfirmations = 2; 
-        ownerSize = 2; // initial size of owners is 2 (admin + second declared on constructor)
+        ownerSize = 2;
     } 
  
     function changeNumConfirmations(uint _requiredConfirmations) public onlyAdmin { 
@@ -62,58 +61,68 @@ contract Multisig{
  
     function removeOwner(address _owner) public onlyAdmin { 
         require(_owner != second, "This address cannot be removed"); 
-        require(isOwner[_owner], "Owner not exist"); // true on isOwner 
+        require(isOwner[_owner], "Owner not exist");
         isOwner[_owner] = false; 
         ownerSize--; 
          
-        // numConfirmations became ownerSize when one owner is removed for security reasons 
         requiredConfirmations = ownerSize; 
     } 
  
     function getBalance() public view returns(uint) { 
-        return address(this).balance; 
+        return address(this).balance;
     }
- 
-    function createTxID() public view onlyAdmin returns(uint) { 
-        uint txID = uint(keccak256(abi.encodePacked(block.timestamp))); 
-        return txID; 
-    } 
- 
-    function confirm(uint _txID) public ownersORadmin(msg.sender) { 
-        require(!isConfirmedBy[msg.sender],"Already confirmed"); 
-        isConfirmedBy[msg.sender] = true; 
-        currentNumConfirmations[_txID]++; 
+
+    function createTxID() public onlyAdmin returns(uint) {
+        require(!isCreateTxIdCalled, "Only one txID can be created");
+        txID = uint(keccak256(abi.encodePacked(block.timestamp)));
+        isCreateTxIdCalled = true;
+        return txID;
+    }
+
+    function confirm() public ownersORadmin(msg.sender) { 
+        require(!isConfirmedBy[msg.sender],"Already confirmed");
+        require(isCreateTxIdCalled == true, "Cannot be called without 'createTxID' at first");
+        isConfirmedBy[msg.sender] = true;
+        currentNumConfirmations[txID]++;
  
         emit Confirmed(msg.sender); 
     } 
  
-    function revoke(uint _txID) public ownersORadmin(msg.sender) { 
+    function revoke() public ownersORadmin(msg.sender) { 
         require(isConfirmedBy[msg.sender], "Not confirmed yet"); 
         isConfirmedBy[msg.sender] = false; 
-        currentNumConfirmations[_txID]--; 
+        currentNumConfirmations[txID]--; 
  
         emit Revoked(msg.sender); 
-    } 
- 
-    function withdrawn(uint _txID, address _to, uint _amount) public payable onlyAdmin { 
-        require(currentNumConfirmations[_txID] == requiredConfirmations, "Not the required number of confirmations"); 
+    }
+
+    function withdrawn(address _to, uint _amount) public payable onlyAdmin { 
+        require(currentNumConfirmations[txID] == requiredConfirmations, "Not the required number of confirmations"); 
  
         (bool success, ) = _to.call{value: _amount}(""); 
-        require(success, "Transaction execution failed"); 
+        require(success, "Transaction execution failed");
  
-        executedTransactions[_txID] = Transactions( 
+        executedTransactions[txID] = Transactions( 
             { 
                 to: _to, 
                 amount: _amount, 
                 time: block.timestamp 
-            } 
-        ); 
- 
+            }
+        );
+
+        isCreateTxIdCalled = false;
+        currentNumConfirmations[txID] = 0;
+        isConfirmedBy[admin] = false;
+
+        for(uint i = 0; i < (ownerSize - 1); i++){
+            
+        }
+
         emit Withdrawn(_to, _amount, block.timestamp); 
-    } 
- 
+    }
+
     receive() external payable { 
         emit Deposit(msg.sender, msg.value, block.timestamp); 
     } 
- 
+
 }
