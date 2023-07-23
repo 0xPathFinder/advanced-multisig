2016ffca52374be375c4941dcda5725dd8dffd55
0xPathfinder <123137984+0xPathFinder@users.noreply.github.com>
2023-07-23 15:27:47 -0700

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -2,13 +2,15 @@
 pragma solidity 0.8.0; 
  
 contract Multisig{ 
- 
-    address public admin;
+
     uint public requiredConfirmations;
-    uint public ownerSize;
+    address admin;
+    uint txID;
+    uint ownerSize;
+    uint numOfAddresses;
     address second;
     bool isCreateTxIdCalled;
-    uint txID;
+    address[] addresses;
 
     mapping(address => bool) public isOwner; 
     mapping(address => bool) public isConfirmedBy; 
@@ -16,7 +18,7 @@ contract Multisig{
     mapping(uint => Transactions) public executedTransactions; 
  
     event Deposit(address indexed _from, uint indexed _value, uint indexed _at); 
-    event Confirmed(address indexed _from); 
+    event Confirmed(address indexed _from);
     event Revoked(address indexed _from);
     event Withdrawn(address indexed _to, uint indexed _value, uint indexed _at); 
  
@@ -43,43 +45,50 @@ contract Multisig{
         isOwner[_owner] = true; 
  
         requiredConfirmations = 2; 
-        ownerSize = 2;
+        ownerSize = 2; // initial size of owners is 2 (admin + second declared on constructor)
     } 
  
-    function changeNumConfirmations(uint _requiredConfirmations) public onlyAdmin { 
+    function changeNumConfirmations(uint _requiredConfirmations) external onlyAdmin { 
         require(_requiredConfirmations != requiredConfirmations, "Already that number of confirmations"); 
         requiredConfirmations = _requiredConfirmations; 
         require(requiredConfirmations >= 2 && requiredConfirmations <= ownerSize, "Number of confirmations should be between two and number of owners"); 
     } 
  
-    function addOwner(address _owner) public onlyAdmin { 
-        require(!isOwner[_owner], "Owner already exist"); 
-        require(ownerSize <= 9, "The maximum number of owners is nine"); 
-        isOwner[_owner] = true; 
-        ownerSize++; 
-    } 
+    function addOwner(address _owner) external onlyAdmin {
+        require(!isOwner[_owner], "Owner already exist");
+        require(ownerSize <= 9, "The maximum number of owners is nine");
+        isOwner[_owner] = true;
+        addresses.push(_owner);
+        numOfAddresses++;
+        ownerSize++;
+    }
  
-    function removeOwner(address _owner) public onlyAdmin { 
-        require(_owner != second, "This address cannot be removed"); 
+    function removeOwner(address _owner) external onlyAdmin {
+        require(_owner != second, "This address cannot be removed");
         require(isOwner[_owner], "Owner not exist");
-        isOwner[_owner] = false; 
-        ownerSize--; 
+        isOwner[_owner] = false;
+        for(uint i = 0; i < numOfAddresses; i++){
+            
+        }
+
+        numOfAddresses--;
+        ownerSize--;
          
         requiredConfirmations = ownerSize; 
     } 
  
-    function getBalance() public view returns(uint) { 
+    function getBalance() external view returns(uint) { 
         return address(this).balance;
     }
 
-    function createTxID() public onlyAdmin returns(uint) {
+    function createTxID() external onlyAdmin returns(uint) {
         require(!isCreateTxIdCalled, "Only one txID can be created");
         txID = uint(keccak256(abi.encodePacked(block.timestamp)));
         isCreateTxIdCalled = true;
         return txID;
     }
 
-    function confirm() public ownersORadmin(msg.sender) { 
+    function confirm() external ownersORadmin(msg.sender) { 
         require(!isConfirmedBy[msg.sender],"Already confirmed");
         require(isCreateTxIdCalled == true, "Cannot be called without 'createTxID' at first");
         isConfirmedBy[msg.sender] = true;
@@ -87,16 +96,17 @@ contract Multisig{
  
         emit Confirmed(msg.sender); 
     } 
- 
-    function revoke() public ownersORadmin(msg.sender) { 
+
+    function revoke() external ownersORadmin(msg.sender) { 
         require(isConfirmedBy[msg.sender], "Not confirmed yet"); 
         isConfirmedBy[msg.sender] = false; 
         currentNumConfirmations[txID]--; 
- 
-        emit Revoked(msg.sender); 
+
+        emit Revoked(msg.sender);
     }
 
-    function withdrawn(address _to, uint _amount) public payable onlyAdmin { 
+    // need to find a way for isConfirmedBy and currentNumConfirmations resetting
+    function withdrawn(address _to, uint _amount) external payable onlyAdmin { 
         require(currentNumConfirmations[txID] == requiredConfirmations, "Not the required number of confirmations"); 
  
         (bool success, ) = _to.call{value: _amount}(""); 
@@ -113,16 +123,19 @@ contract Multisig{
         isCreateTxIdCalled = false;
         currentNumConfirmations[txID] = 0;
         isConfirmedBy[admin] = false;
+        isConfirmedBy[second] = false;
 
-        for(uint i = 0; i < (ownerSize - 1); i++){
-            
+       for(uint i = 0; i < numOfAddresses; i++){
+            isConfirmedBy[addresses[i]] = false;
         }
 
+        numOfAddresses = 0;
+
         emit Withdrawn(_to, _amount, block.timestamp); 
-    }
+    } 
 
     receive() external payable { 
         emit Deposit(msg.sender, msg.value, block.timestamp); 
-    } 
+    }
 
 }
